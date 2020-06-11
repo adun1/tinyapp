36965f9460272118cd0b7a15e124d680a43f90a0
adun1 <bmccormack15@gmail.com>
2020-06-11 04:34:04 +0000

    added random function and writing key to urlDatabase (in memory)

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = 8080;
 
@@ -10,6 +11,12 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//testing function required
+function generateRandomString() {
+  let temp = uuidv4().split('-')[0].slice(2,10);
+  return temp;
+}
 
 app.get('/', (req, res) => {
   res.send("Hello!");
@@ -39,8 +46,11 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>");
 });
 
+//note not checking for existing urls yet before adding
 app.post('/urls', (req, res) => {
-  console.log(req.body);
+  const key = generateRandomString()
+  urlDatabase[key] = req.body.longURL;
+  console.log(urlDatabase);
   res.send('Ok');
 });
 
